082d6f257b903379a37e2dd3d19dc3a93c2c0685
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-10-23 17:56:59 +0530

    4️⃣ticket model

--- app/{models}/Ticket.js
+++ app/{models}/Ticket.js
@@ -1,22 +1,22 @@
 import mongoose, { Schema } from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 
 const TicketSchema = new Schema(
-    {
-        title: String,
-        description: String,
-        category: String,
-        priority: Number,
-        status: String,
-        progress: Number,
-        active: Boolean,
-    },
-    {
-        timestamps: true,
-    }
-)
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    status: String,
+    progress: Number,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Ticket = mongoose.model.Ticket || mongoose.model("Ticket", TicketSchema);
 export default Ticket;
