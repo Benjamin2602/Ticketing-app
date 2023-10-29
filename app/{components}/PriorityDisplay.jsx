7248b5cabd5d96d2708dedd322c352bcbb64393d
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-10-29 20:36:24 +0530

    5️⃣ updated the title,priority,des in the client

--- app/{components}/PriorityDisplay.jsx
+++ app/{components}/PriorityDisplay.jsx
@@ -2,12 +2,37 @@ import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-const PriorityDisplay = () => {
+const PriorityDisplay = ({ priority }) => {
   return (
     <div className="flex justify-start align-baseline">
-      <FontAwesomeIcon icon={faFire} className="text-orange-400" />
-      <FontAwesomeIcon icon={faFire} className="text-orange-400" />
-      <FontAwesomeIcon icon={faFire} className="text-orange-400" />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={` pr-1 ${
+          priority > 0 ? " text-orange-400" : " text-slate-400"
+        }`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={` pr-1 ${
+          priority > 1 ? " text-orange-400" : " text-slate-400"
+        }`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`  pr-1 ${
+          priority > 2 ? " text-orange-400" : " text-slate-400"
+        }`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={` pr-1 ${
+          priority > 3 ? " text-orange-400" : " text-slate-400"
+        }`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={` ${priority > 4 ? " text-orange-400" : " text-slate-400"}`}
+      />
     </div>
   );
 };
--- app/{components}/TicketCard.jsx
+++ app/{components}/TicketCard.jsx
@@ -4,26 +4,22 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressBarDisplay from "./ProgressBarDisplay";
 import StatusDisplay from "./StatusDisplay";
 
-const TicketCard = () => {
+const TicketCard = ({ ticket }) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md p-3 m-2">
       <div className="flex mb-3">
-        <PriorityDisplay />
+        <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
           <DeleteBlock />
         </div>
       </div>
-      <h4>Ticket Title</h4>
+      <h4>{ticket.title}</h4>
       <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">
-        This is the ticket description! Please do this ticket
-      </p>
+      <p className="whitespace-pre-wrap">{ticket.description}</p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">
-            23/10/23 4:07pm
-          </p>
+          <p className="text-xs my-1">23/10/23 4:07pm</p>
           <ProgressBarDisplay />
         </div>
         <div className="ml-auto flex items-end">
