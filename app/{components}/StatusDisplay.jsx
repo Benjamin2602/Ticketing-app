61fed4c9e02ddb490ae48560fb9bc5115956ad5e
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-10-29 21:11:16 +0530

    5️⃣ customizing the status Color

--- app/{components}/StatusDisplay.jsx
+++ app/{components}/StatusDisplay.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 
+
 const StatusDisplay = ({status}) => {
+  const getColor = (status) =>{
+    let color = "bg-gradient-to-r from-green-400 via-green-500 to-green-600";
+    switch(status.toLowerCase())
+{
+  case  "completed":
+    color = "bg-gradient-to-r from-teal-400  to-lime-600";
+    return color;
+  case  "in progress":
+    color = "bg-gradient-to-r from-pink-400  to-pink-800";
+    return color;
+  case  "started":
+    color = "bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600";
+    return color;
+  case  "not started":
+    color = "bg-gradient-to-r from-orange-400 to-red-700";
+    return color;
+  default:
+    color = "bg-gradient-to-r from-green-400 via-green-500 to-green-600";
+    return color;
+
+
+}  
+  }
   return (
-    <span className="bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-xs px-2 py-1 text-center mr-2 mb-2">
+    <span className={` font-medium rounded-lg text-xs px-2 py-1 text-center mr-2 mb-2 ${getColor(status)}`}>
       {status}
     </span>
   );
