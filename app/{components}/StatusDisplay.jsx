5dfe80e318ffce5c1bfec3e39a7040b5e57d942d
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-10-29 21:23:57 +0530

    5️⃣ fixed the TimeStamp

--- app/{components}/StatusDisplay.jsx
+++ app/{components}/StatusDisplay.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 
-
-const StatusDisplay = ({status}) => {
-  const getColor = (status) =>{
+const StatusDisplay = ({ status }) => {
+  const getColor = (status) => {
     let color = "bg-gradient-to-r from-green-400 via-green-500 to-green-600";
-    switch(status.toLowerCase())
-{
-  case  "completed":
-    color = "bg-gradient-to-r from-teal-400  to-lime-600";
-    return color;
-  case  "in progress":
-    color = "bg-gradient-to-r from-pink-400  to-pink-800";
-    return color;
-  case  "started":
-    color = "bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600";
-    return color;
-  case  "not started":
-    color = "bg-gradient-to-r from-orange-400 to-red-700";
-    return color;
-  default:
-    color = "bg-gradient-to-r from-green-400 via-green-500 to-green-600";
-    return color;
-
-
-}  
-  }
+    switch (status.toLowerCase()) {
+      case "completed":
+        color = "bg-gradient-to-r from-teal-400  to-lime-600";
+        return color;
+      case "in progress":
+        color = "bg-gradient-to-r from-pink-400  to-pink-800";
+        return color;
+      case "started":
+        color = "bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600";
+        return color;
+      case "not started":
+        color = "bg-gradient-to-r from-orange-400 to-red-700";
+        return color;
+      default:
+        color = "bg-gradient-to-r from-green-400 via-green-500 to-green-600";
+        return color;
+    }
+  };
   return (
-    <span className={` font-medium rounded-lg text-xs px-2 py-1 text-center mr-2 mb-2 ${getColor(status)}`}>
+    <span
+      className={` font-medium rounded-lg text-xs px-2 py-1 text-center mr-2 mb-2 ${getColor(
+        status
+      )}`}
+    >
       {status}
     </span>
   );
